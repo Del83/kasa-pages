bfff6b8d0ba589e533336042b197874edc545b32
Del83 <91467158+Del83@users.noreply.github.com>
2023-01-04 18:42:04 +0100

    Route

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route exact path="/home" element={<Home />}></Route>
-      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/kasa_pages" element={<Home />}></Route>
       <Route path="/lodging/:id" element={<Lodging />}></Route>
       <Route exact path="/about" element={<About />}></Route>
       <Route path="*" element={<Error />}></Route>

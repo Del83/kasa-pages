c51ac307a720dc6f75ee23ee0545d30573d9d521
Del83 <91467158+Del83@users.noreply.github.com>
2023-01-04 18:56:28 +0100

    router

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <HashRouter>
     <Routes>
       <Route exact path="/home" element={<Home />}></Route>
-      <Route exact path="/kasa_pages" element={<Home />}></Route>
+      <Route exact path="/kasa-pages" element={<Home />}></Route>
       <Route exact path="/" element={<Home />}></Route>
       <Route path="/lodging/:id" element={<Lodging />}></Route>
       <Route exact path="/about" element={<About />}></Route>
